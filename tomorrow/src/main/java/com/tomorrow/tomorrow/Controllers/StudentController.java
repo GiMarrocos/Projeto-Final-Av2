ee5332c3ff4ea4b215685277d42a41b9a472972b
Edson Junior <edsonjn2013@gmail.com>
2022-11-21 23:13:28 -0300

    feat: create put and delete methods in StudentController.

--- tomorrow/src/main/java/com/tomorrow/tomorrow/Controllers/StudentController.java
+++ tomorrow/src/main/java/com/tomorrow/tomorrow/Controllers/StudentController.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tomorrow.tomorrow.entities.Student;
 import com.tomorrow.tomorrow.services.StudentService;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping(path = "/student")
 public class StudentController {
 
     @Autowired
@@ -38,6 +40,19 @@ public class StudentController {
     public ResponseEntity<Student> createNewStudent(@RequestBody Student student){
         student = studentService.save(student);
         return ResponseEntity.ok().body(student);
+    }
+    
+    @RequestMapping(value = {"/{id}"}, method = RequestMethod.PUT )
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student,@PathVariable long id){
+        student = studentService.updateStudent(student, id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping(path = {"/{id}"})
+    public ResponseEntity<?> deleteStudent(@PathVariable long id){
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
