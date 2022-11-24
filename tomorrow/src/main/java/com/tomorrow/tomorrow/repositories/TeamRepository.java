f14176e0ef2ce88907de0d21d66615c910e01a41
Mateus Henrique <mateushcandido5@gmail.com>
2022-11-23 23:01:27 -0300

    TeamRepository implementation

---
+++ tomorrow/src/main/java/com/tomorrow/tomorrow/repositories/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.tomorrow.tomorrow.repositories;
+
+import com.tomorrow.tomorrow.entities.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
