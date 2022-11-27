26362f81775b2a2b4ba1daaa7d44af9814e4a060
Mateus Henrique <mateushcandido5@gmail.com>
2022-11-27 20:57:58 -0300

    Adjustment in the Course entity. Triggering enums attributes on days of the week

--- tomorrow/src/main/java/com/tomorrow/tomorrow/entities/Course.java
+++ tomorrow/src/main/java/com/tomorrow/tomorrow/entities/Course.java
@@ -1,6 +1,7 @@
 package com.tomorrow.tomorrow.entities;
 
 
+import com.tomorrow.tomorrow.entities.enums.ClassStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,6 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @Table
@@ -37,23 +37,89 @@ public class Course {
     private Double value;
 
     @Column(nullable = false)
-    private Boolean monday;
+    private Integer monday;
 
     @Column(nullable = false)
-    private Boolean tuesday;
+    private Integer tuesday;
 
     @Column(nullable = false)
-    private Boolean wednesday;
+    private Integer wednesday;
 
     @Column(nullable = false)
-    private Boolean thursday;
+    private Integer thursday;
 
     @Column(nullable = false)
-    private Boolean friday;
+    private Integer friday;
 
     @Column(nullable = false)
-    private Boolean saturday;
+    private Integer saturday;
 
+
+    public Course(Long courseId, String course_name, Double classWorkload, Double totalWorkload, String shift,
+                  Double value, ClassStatus monday, ClassStatus tuesday, ClassStatus wednesday,
+                  ClassStatus thursday, ClassStatus friday, ClassStatus saturday) {
+        this.courseId = courseId;
+        this.course_name = course_name;
+        this.classWorkload = classWorkload;
+        this.totalWorkload = totalWorkload;
+        this.shift = shift;
+        this.value = value;
+        setMonday(monday);
+        setThursday(tuesday);
+        setWednesday(wednesday);
+        setThursday(thursday);
+        setFriday(friday);
+        setSaturday(saturday);
+    }
+
+
+    public ClassStatus getMonday() {
+        return ClassStatus.valueOf(monday);
+    }
+
+    public void setMonday(ClassStatus monday) {
+        this.monday = monday.getStatus_code();
+    }
+
+    public ClassStatus getTuesday() {
+        return ClassStatus.valueOf(tuesday);
+    }
+
+    public void setTuesday(ClassStatus tuesday) {
+        this.tuesday = tuesday.getStatus_code();
+    }
+
+    public ClassStatus getWednesday() {
+        return ClassStatus.valueOf(wednesday);
+    }
+
+    public void setWednesday(ClassStatus wednesday) {
+        this.wednesday = wednesday.getStatus_code();
+    }
+
+    public ClassStatus getThursday() {
+        return ClassStatus.valueOf(thursday);
+    }
+
+    public void setThursday(ClassStatus thursday) {
+        this.thursday = thursday.getStatus_code();
+    }
+
+    public ClassStatus getFriday() {
+        return ClassStatus.valueOf(friday);
+    }
+
+    public void setFriday(ClassStatus friday) {
+        this.friday = friday.getStatus_code();
+    }
+
+    public ClassStatus getSaturday() {
+        return ClassStatus.valueOf(saturday);
+    }
+
+    public void setSaturday(ClassStatus saturday) {
+        this.saturday = saturday.getStatus_code();
+    }
 
     @Override
     public boolean equals(Object o) {
